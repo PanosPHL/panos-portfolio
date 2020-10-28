83ee263d67cfa228c799f03c4b4ffd6dc1649a0f
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-10-28 11:01:13 -0400

    Portolio template built from open source Gatsby project https://www.gatsbyjs.com/plugins/@wkocjan/gatsby-theme-intro/

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    siteMetadata: {
+      description: "Portfolio page of Panayiotis Dimopoulos",
+      locale: "en",
+      title: "Introducing Panayiotis Dimopoulos"
+    },
+    plugins: [
+      {
+        resolve: "@wkocjan/gatsby-theme-intro",
+        options: {
+          theme: "dark-pink",
+          basePath: '/',
+          contentPath: 'content/',
+          showThemeLogo: false
+        },
+      },
+    ],
+  }
